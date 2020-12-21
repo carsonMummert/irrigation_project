c8dd1753d4281c0dbebdc455d4b6afcecb421bd8
carsonMummert <carsonmummert@gmail.com>
2020-12-21 19:04:57 +0000

    start creating graphing functions

--- irrigation_project.py
+++ irrigation_project.py
@@ -4,31 +4,7 @@ import curses
 from curses import wrapper
 import time
 from datetime import datetime
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
-
-# setup of mcp3008 adc, supports up to 8 sensors
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)
-mcp = MCP.MCP3008(spi, cs)
-chan0 = AnalogIn(mcp, MCP.P0)
-chan1 = AnalogIn(mcp, MCP.P1)
-chan2 = AnalogIn(mcp, MCP.P2)
-chan3 = AnalogIn(mcp, MCP.P3)
-
-bar = '█' # an extended ASCII 'fill' character
-stdscr = curses.initscr() # create curses screen
-
-# records soil values, which are being read continuously 
-def get_sensor_data():
-	ch0 = chan0.voltage
-	ch1 = chan1.voltage
-	ch2 = chan2.voltage
-	ch3 = chan3.voltage
-	return ch0, ch1, ch2, ch3
+from soil_sensor import *
 
 def load_soil_data(n=7): # returns n days worth of data 
 	with open('soil_data.txt', 'r') as soil_data:
@@ -44,7 +20,15 @@ def load_soil_data(n=7): # returns n days worth of data
 
 	return last_n, (len(last_n))
 	
+bar = '█' # an extended ASCII 'fill' character
+stdscr = curses.initscr() # create curses screen
 		
+def make_graph(window, dates, data, y_start=1, x_start=1):
+	# data => y range 1-10
+	
+	window.border(0)
+	pass
+	
 def main(stdscr):
 	height, width = stdscr.getmaxyx() # get the window size
 	curses.start_color()
@@ -79,11 +63,7 @@ def main(stdscr):
 		s2_data.append(line[3])
 		s3_data.append(line[4])
 		s4_data.append(line[5])
-	# stdscr.addstr(height-3,1,s1_data[0])
-
-	# stdscr.addstr(height-3,1,last_n[len(last_n)-1][0])
-	# stdscr.addstr(height-4,1,str(days_old))
-	stdscr.addstr(height-20,1,str(days_old))
+	stdscr.addstr(height-3,1,str(days_old))
 
 	# gregarious monstera ascii art
 	with open('med_monstera.txt', 'r') as leaves:
@@ -110,81 +90,88 @@ def main(stdscr):
 
 	# Use the 'q' key to quit
 	k = 0
-	written_to = False
-	while (k != ord('q')):
-		ch0_voltage, ch1_voltage, ch2_voltage, ch3_voltage = get_sensor_data() # get the data values
-		# these will hopefully cutdown on times that we render the graphs
-		if prev_ch0 != ch0_voltage:
-			prev_ch0 = ch0_voltage
-			win1.clear()
+	written_to_today = False
+	while True:
+		if k != ord('q'):
+			# read data values
+			ch0_voltage, ch1_voltage, ch2_voltage, ch3_voltage = get_sensor_data()
 
-		if prev_ch1 != ch1_voltage:
+			# these will hopefully cutdown on times that we render the graphs
+			if prev_ch0 != ch0_voltage:
+				prev_ch0 = ch0_voltage
+				win1.clear()
 
-			prev_ch1 = ch1_voltage
-			win2.clear()
+			if prev_ch1 != ch1_voltage:
 
-		if prev_ch2 != ch2_voltage:
-			prev_ch2 = ch2_voltage
-			win3.clear()
+				prev_ch1 = ch1_voltage
+				win2.clear()
 
-		if prev_ch3 != ch3_voltage:
-			prev_ch3 = ch3_voltage
-			win4.clear()
+			if prev_ch2 != ch2_voltage:
+				prev_ch2 = ch2_voltage
+				win3.clear()
 
-		now = datetime.now()
-		stdscr.addstr(1,width-10,now.strftime("%x"), curses.color_pair(1))
-		# write to csv once a day
-		# not sure the logic for this checks out...
-		# the written_to bool counts 
-		if len(last_n) != 0 and written_to == False:
-			if last_n[len(last_n)-1][0] != now.strftime("%x"):
-				with open('soil_data.txt', 'a') as soil_data:
-						soil_data.write(now.strftime("%x") + ',' 
-							+ now.strftime("%X") + ','
-							+ str(ch0_voltage) + ',' + str(ch1_voltage) + ',' 
-							+ str(ch2_voltage) + ',' + str(ch3_voltage) + "\n")
-				soil_data.close()
-				written_to = True
+			if prev_ch3 != ch3_voltage:
+				prev_ch3 = ch3_voltage
+				win4.clear()
 
-		win1.border(0)
-		win2.border(0)
-		win3.border(0)
-		win4.border(0)
-		
-		date_min = dates[0]
-		date_max = dates[len(dates)-1]
-		stdscr.addstr(13,13,date_min, curses.color_pair(1))
-		stdscr.addstr(13,43,date_max, curses.color_pair(1))
-		
-		stdscr.addstr(4,48,max(s1_data)[0:5] + "V")
-		stdscr.addstr(11,48,min(s1_data)[0:5] + "V")
+			now = datetime.now()
+			stdscr.addstr(1,width-10,now.strftime("%x"), curses.color_pair(1))
+			# write to csv once a day
+			# not sure the logic for this checks out...
+			if len(last_n) != 0 and written_to_today == False:
+				if last_n[len(last_n)-1][0] != now.strftime("%x"):
+					with open('soil_data.txt', 'a') as soil_data:
+							soil_data.write(now.strftime("%x") + ',' 
+								+ now.strftime("%X") + ','
+								+ str(ch0_voltage) + ',' + str(ch1_voltage) + ',' 
+								+ str(ch2_voltage) + ',' + str(ch3_voltage) + "\n")
+					soil_data.close()
+					written_to = True
 
-		ch0_scaled = ((ch0_voltage - 0) / (4 - 0)) * (30 - 0) + 0
-		ch1_scaled = ((ch1_voltage - 0) / (4 - 0)) * (30 - 0) + 0
-		ch2_scaled = ((ch2_voltage - 0) / (4 - 0)) * (30 - 0) + 0
-		ch3_scaled = ((ch3_voltage - 0) / (4 - 0)) * (30 - 0) + 0
+			make_graph(win1, dates, s1_data)
+			win2.border(0)
+			win3.border(0)
+			win4.border(0)
+			
+			date_min = dates[0]
+			date_max = dates[len(dates)-1]
+			stdscr.addstr(13,13,date_min, curses.color_pair(1))
+			stdscr.addstr(13,43,date_max, curses.color_pair(1))
+			
+			stdscr.addstr(4,48,max(s1_data)[0:5] + "V")
+			stdscr.addstr(11,48,min(s1_data)[0:5] + "V")
 
-		# create bars bases on the returned values
-		win1.addstr(1, 1, bar * int(ch0_scaled), curses.color_pair(2))
-		win1.refresh()
-		win2.addstr(1, 1, bar * int(ch1_scaled), curses.color_pair(3))
-		win2.refresh()
-		win3.addstr(1, 1, bar * int(ch2_scaled), curses.color_pair(2))
-		win3.refresh()
-		win4.addstr(1, 1, bar * int(ch3_scaled), curses.color_pair(3))
-		win4.refresh()
-		
-		# add numeric values beside the bars (we get 16 places of precision, but round
-		# em off to be pretty
-		stdscr.addstr(8,1,"Curr. Val.:")
-		stdscr.addstr(9,1, str(round(ch0_voltage,4)) + "V ",curses.A_BOLD )
-		stdscr.addstr(15,50, str(round(ch1_voltage,4)) + "V ",curses.A_BOLD )
-		stdscr.addstr(19,50, str(round(ch2_voltage,4)) + "V ",curses.A_BOLD )
-		stdscr.addstr(23,50, str(round(ch3_voltage,4)) + "V ",curses.A_BOLD )
-		stdscr.refresh()
-		time.sleep(1)
-		stdscr.nodelay(1)
-		k = stdscr.getch() # look for a keyboard input, but don't wait
+			ch0_scaled = ((ch0_voltage - 0) / (4 - 0)) * (30 - 0) + 0
+			ch1_scaled = ((ch1_voltage - 0) / (4 - 0)) * (30 - 0) + 0
+			ch2_scaled = ((ch2_voltage - 0) / (4 - 0)) * (30 - 0) + 0
+			ch3_scaled = ((ch3_voltage - 0) / (4 - 0)) * (30 - 0) + 0
+
+			# create bars bases on the returned values
+			win1.addstr(1, 1, bar * int(ch0_scaled), curses.color_pair(2))
+			win1.refresh()
+			win2.addstr(1, 1, bar * int(ch1_scaled), curses.color_pair(3))
+			win2.refresh()
+			win3.addstr(1, 1, bar * int(ch2_scaled), curses.color_pair(2))
+			win3.refresh()
+			win4.addstr(1, 1, bar * int(ch3_scaled), curses.color_pair(3))
+			win4.refresh()
+			
+			# add numeric values beside the bars (we get 16 places of precision, but round
+			# em off to be pretty
+
+			stdscr.addstr(8,1,"Curr. Val.:")
+			stdscr.addstr(9,1, str(round(ch0_voltage,4)) + "V",curses.A_BOLD )
+			
+			stdscr.addstr(14,1, "Curr. Val.:")
+			stdscr.addstr(15,1, str(round(ch1_voltage,4)) + "V",curses.A_BOLD )
+			stdscr.addstr(19,50, str(round(ch2_voltage,4)) + "V",curses.A_BOLD )
+			stdscr.addstr(23,50, str(round(ch3_voltage,4)) + "V",curses.A_BOLD )
+			stdscr.refresh()
+			time.sleep(1)
+			stdscr.nodelay(1)
+			k = stdscr.getch() # look for a keyboard input, but don't wait
+		elif k == ord('q'):
+			break
 
 wrapper(main)
 curses.endwin() # restore the terminal settings back to the original
